464a28f2c548db948cf605784fc9e9f3743bf64b
johhns <johhns@hotmail.com>
2016-01-30 13:17:29 -0600

    Initial Commit

---
+++ MundoHamburguesa/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MundoHamburguesa
+//
+//  Created by Juan  Sanchez on 30/1/16.
+//  Copyright © 2016 Juan  Sanchez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
